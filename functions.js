85e30305059fe4ddcbcbb02340a0b02f41f563a2
eshe-aziz <eshehaledhaziz@gmail.com>
2024-03-29 11:37:30 +0300

    added a file

---
+++ functions.js
@@ -0,0 +1,50 @@
+/// Given an array of strings, use a function to reverse all the elements in the string in ascending order and the specific elements in descending order
+function func() {
+    const arr = ["Apple", "Banana", "Pawpaw", "Melon"]
+    console.log(arr)
+
+    const new_arr = arr.reverse()
+    console.log(new_arr)
+}
+func()
+
+// Using JS functions and an array of numbers, return positive if an element within the array is positive, negative if an element is negative, else zero
+function numbers() {
+    const num = [1, 3, -4, -6, 9, -5]
+    
+    if (num == 1)
+    console.log("positive")
+    else
+    console.log("negative")
+}
+numbers()
+
+
+// Given an array of objects, where each object represents an employee with an id, name, and salary property,
+//write a function that returns a new array of employee objects sorted by their salary in ascending order.
+
+
+
+
+
+
+
+
+// Write a function that accepts an array of numbers and uses the forEach() method to console.log each number multiplied by 2.
+function multiply() {
+    const num = [20, 30, 40]
+    num.forEach((number) => {
+        console.log(number * 2)
+    })
+}
+multiply()
+
+
+// Write a function that takes in an array of numbers and consoles the first four items multiplied by 8 and the last two added by 5.
+//Console the array with the new values
+
+
+
+
+
+
